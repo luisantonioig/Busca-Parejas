d11d097f0495d08d276f995ba1e73a6b1414a753
manuel <cacahute_america@hotmail.com>
2016-04-27 09:26:45 -0500

    ESTA ES LA CONEXIÓN
    conexión para la base de datos aplica para todos

---
+++ conectar.php
@@ -0,0 +1,9 @@
+<?php
+$conexion = mysql_connect("localhost", "root", "");
+        //servidor, administrador y password
+        if (!$conexion) {
+            echo "No se puede establecer una conexion";
+        } else {
+            //echo "CONEXION EXITOSA!!!";
+        }
+ ?>      
