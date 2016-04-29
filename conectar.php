d080828a4b78a0d4353c8803f906d8f110c2ed72
FloresEve <everardojacobo1@hotmail.com>
2016-04-29 11:53:19 -0500

    Elimado todo
    ok

--- conectar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = mysql_connect("localhost", "root", "");
-        //servidor, administrador y password
-        if (!$conexion) {
-            echo "No se puede establecer una conexion";
-        } else {
-            //echo "CONEXION EXITOSA!!!";
-        }
- ?>      
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "Hola a todos";
-?>
